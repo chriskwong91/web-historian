ebbe8b5a5b2aa8e2c8b07b0478715b27f81245a3
chriskwong91 <chriskwong91@gmail.com>
2016-08-17 15:31:35 -0700

    Complete tests #1-4

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,47 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var url = require('url');
+var fs = require('fs');
+var helpers = require('./http-helpers');
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  console.log('Method: ', req.method, 'URL: ', req.url);
+  var parsedUrl = url.parse(req.url);
+
+  if (req.method === 'GET') {
+    if (parsedUrl.pathname === '/') {
+      helpers.serveAssets(res, '/index.html');
+    } else if (parsedUrl.pathname === '/styles.css') {
+      helpers.serveAssets(res, '/styles.css');
+    } else {
+      var filePath = archive.paths.archivedSites + parsedUrl.pathname;
+      fs.exists(filePath, function(exists) {
+        if (exists) {
+          fs.readFile(filePath, function(error, data) {
+            res.end(data);
+          });
+        } else {
+          res.writeHead(404, null);
+          res.end('Not Found');
+        }
+      });
+    }
+  } else if (req.method === 'POST') {
+    var site = '';
+    req.on('data', function(chunk) {
+      site += chunk;
+    });
+
+    req.on('end', function() {
+      site = site.split('=');
+      fs.appendFile(archive.paths.list, site[1] + '\n', function(err) {
+        if (err) { throw err; }
+        res.writeHead(302, null);
+        res.end();
+      });
+    });
+
+  }
+  // res.end(archive.paths.list);
 };
